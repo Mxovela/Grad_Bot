85e7a017ff17c32617e5d558ba3a2a8357af9a66
Mxovela <yanela692@gmail.com>
2026-01-28 14:50:26 +0200

    no changes

--- backend/email_service.py
+++ backend/email_service.py
@@ -39,6 +39,12 @@ def get_all_graduate_emails():
         print(f"Error fetching graduate emails: {e}")
         return []
 
+    # response = supabase.rpc("get_graduate_emails").execute()
+
+    emails = [row["email"] for row in response.data]
+
+    return response.data
+
 def get_graduate_email_by_id(graduate_id: str):
     """Fetch email for a specific graduate ID"""
     try:
@@ -101,4 +107,4 @@ def send_email(to_emails: list[str], subject: str, body: str):
         print(f"❌ Error sending email: {e}")
         
 #send_email(["mxovelamxo@outlook.com","mo1motala@gmail.com"], "Testing function send", "test4")
-#print(get_all_graduate_emails())
+print(get_all_graduate_emails())
