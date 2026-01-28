3acd082269ce3b15c6b49a857d6e27303a8710c5
MoMuhammad <mo1motala@gmail.com>
2026-01-28 22:15:30 +0200

    Improved bot functionality

--- backend/cloud_chat.py
+++ backend/cloud_chat.py
@@ -5,39 +5,41 @@ from datetime import datetime
 from pypdf import PdfReader
 from docx import Document
 from pptx import Presentation
-from supabase import create_client
 from openai import OpenAI
-from supabase.client import Client
 from dotenv import load_dotenv
+from supabase_client import supabase
  
 load_dotenv()
  
-# Set up Supabase client
-url: str = os.getenv("SUPABASE_URL")
-key: str = os.getenv("SUPABASE_SERVICE_KEY")
-supabase: Client = create_client(url, key)
- 
 # Helps size text chunks based on token count
-tokenizer = tiktoken.get_encoding("cl100k_base")
+tokenizer = tiktoken.get_encoding("o200k_base")
  
 # Embedding parameters
-CHUNCK_SIZE = 300
-CHUNCK_OVERLAP = 70
-RETRIEVAL_LIMIT = 3
+CHUNCK_SIZE = 180
+CHUNCK_OVERLAP = 40
+
+# Quering parameters
+NO_PREVIOUS_QS = 3
+LIMIT_HISTORY = 2*NO_PREVIOUS_QS # Must be even
+
+# Sources parameters
+RETRIEVAL_LIMIT = 10
+QUERY_LIMIT = 5
+CONTEXT_TOKEN_BUDGET = 2500
  
 # Set up OpenAI client and parameters
 client = OpenAI()
- 
-NO_PREVIOUS_QS = 3
-LIMIT_HISTORY = 2*NO_PREVIOUS_QS # Must be even
-CONTEXT_TOKEN_BUDGET = 2500
+
+TEMP = 0.2
+SIMILARITY_THRESHOLD = 0.1
 PROMPT = (
         "You are a helpful assistant.\n"
         "Answer the user's question using ONLY the provided document context.\n"
+        "You may expand on the answer using the provided context.\n"
         "If the answer is not present in the context, say you do not know.\n"
     )
  
-TEST = False
+TEST = True
  
 ## Train context
 #/ Locally store and process the file to extract text
@@ -107,7 +109,6 @@ def index_document(document_id: str, file_path: str):
 
     text = extract_text(file_path)
     chunks = chunk_text(text)
-   
  
     # 1. Delete old chunks
     supabase.table("document_chunks") \
@@ -124,7 +125,7 @@ def index_document(document_id: str, file_path: str):
             "content": chunk,
             "embedding": embed_text(chunk),
         })
-
+ 
     r = supabase.table("document_chunks").insert(rows).execute()
  
 #/ Batch embedding
@@ -161,34 +162,77 @@ def question_history(chat_id):
     previous_questions = [ r["content"] for r in user_history_resp.data]
  
     return previous_questions
- 
+
+def detect_query_intent(question: str) -> str:
+    q = question.lower().strip()
+
+    if q.startswith(("what is", "what’s", "what are", "define", "explain")):
+        return "definition"
+
+    if q.startswith(("how","how does", "how do", "how is", "how can")):
+        return "mechanism"
+
+    if q.startswith(("why",)):
+        return "reasoning"
+
+    if any(opt in q for opt in ["compare", "difference", "vs"]):
+        return "comparison"
+
+    return "general"
+
 def rewrite_query(previous_questions, current_question) -> str:
  
+    intent = detect_query_intent(current_question)
+
     if not previous_questions:
         return current_question
- 
+
     history = "\n".join(previous_questions[::-1])
- 
+
+    if intent == "definition":
+        instruction = (
+            "Rewrite the user's question as a concise technical definition query.\n"
+            "Include the product or concept category and its primary purpose.\n"
+            "Do NOT include conversational phrasing.\n"
+        )
+
+    elif intent == "mechanism":
+        instruction = (
+            "Rewrite the user's question as a technical explanation query.\n"
+            "Focus on how the system works or operates.\n"
+        )
+
+    elif intent == "comparison":
+        instruction = (
+            "Rewrite the user's question as a comparison query.\n"
+            "Explicitly name the entities being compared.\n"
+        )
+
+    else:
+        instruction = (
+            "Rewrite the user's latest question into a standalone, explicit search query\n"
+            "that can be used to retrieve relevant document passages.\n"
+        )
+
     prompt = f"""
-        Rewrite the user's latest question into a standalone, explicit search query
-        that can be used to retrieve relevant document passages.
- 
+        Given the conversation history and the latest user question, {intent} intent,
+        {instruction}
+
         Conversation (user questions only):
         {history}
- 
+
         Latest question:
         {current_question}
- 
+
         Standalone search query:
         """.strip()
- 
-    response = client.chat.completions.create(
+
+    response = client.responses.create(
         model="gpt-4o-mini",
-        temperature=0,
-        messages=[{"role": "user", "content": prompt}]
+        input=prompt
     )
- 
-    return response.choices[0].message.content.strip()
+
+    return response.output_text.strip()
  
 #/ Sources
 def retrieve_chunks(query_embedding, limit=RETRIEVAL_LIMIT):
@@ -199,8 +243,26 @@ def retrieve_chunks(query_embedding, limit=RETRIEVAL_LIMIT):
             "match_count": limit
         }
     ).execute()
- 
-    return response.data
+
+    if not response.data:
+        return []
+
+    # Filter by similarity threshold
+    filtered = []
+    for r in response.data:
+
+        print("file: ",r["file_name"]," similarity: ",r["similarity"])
+
+        if r["similarity"] >= SIMILARITY_THRESHOLD:
+            filtered.append(r)
+
+    if not filtered: filtered = [r for r in response.data if r["similarity"] >= 0]  # Ensure at least one chunk is returned
+    if not filtered: filtered = [response.data[0]]  # Fallback to at least one chunk if none meet criteria
+
+    if len(filtered) > QUERY_LIMIT:
+        filtered = filtered[:QUERY_LIMIT]
+
+    return filtered
  
 def trim_chunks_by_tokens(chunks: list[dict]) -> list[dict]:
     total_tokens = 0
@@ -244,7 +306,7 @@ def chunks_by_id(chunks: list[str]) -> list[dict]:
     return sources
 
 #/ Chat history
-def ordered_history(user_id, limit=LIMIT_HISTORY):
+def ordered_history(user_id, limit=100):
  
     chat_id = get_chat_id(user_id)
    
@@ -269,8 +331,8 @@ def context_history(chat_id, limit=LIMIT_HISTORY):
         supabase.table("chat_messages")
         .select("role, content")
         .eq("chat_id", chat_id)
-        .order("id")
-        .limit(LIMIT_HISTORY)
+        .order("id", desc=True)
+        .limit(limit)
         .execute()
     )
  
@@ -308,9 +370,10 @@ def get_chat_id(user_id):
         .eq("user_id", user_id)
         .execute()
         ).data
+    
+    if not chat_id:
+        return None
    
-    if not chat_id: return None
-        
     return chat_id[0]["id"]
  
 def new_chat(user_id):
@@ -318,9 +381,12 @@ def new_chat(user_id):
         supabase.table("chats")
         .insert({"user_id": user_id})
         .execute()
-        ).data[0]["id"]
+        ).data
+    
+    if not chat_id:
+        return None
    
-    return chat_id
+    return chat_id[0]["id"]
  
 def delete_chat(chat_id):
     response = (
@@ -340,40 +406,35 @@ def new_chat_on_token(user_id):
  
 ## Chat function to answer questions based on document context
 def chat(user_id, question):
-    chat_existed = False
+ 
     local_time = datetime.now().strftime('%I:%M:%S %p')
-
+ 
     chat_id = get_chat_id(user_id)
-    
+ 
     if not chat_id:
         chat_id = new_chat(user_id)
-        previous_questions = []
-    else:
-        previous_questions = question_history(chat_id)
+
+    if not chat_id:
+        return {"error": "Could not create chat session."}
  
     ##QUESTIONS
     #/ improve question
-    rewritten_question = rewrite_query(previous_questions, question)
- 
-    #/ store question
-    question_tokens = len(tokenizer.encode(question))
+    previous_questions = question_history(chat_id)
 
-    q = supabase.table("chat_messages").insert({
-        "chat_id": chat_id,
-        "role": "user",
-        "content": question,
-        "token_count": question_tokens,
-        "time_stamp": local_time
-    }).execute()
+    if not previous_questions: previous_questions = [question]
+ 
+    rewritten_question = rewrite_query(previous_questions, question)
+    if TEST:
+        print("previous_questions: ","\n".join(previous_questions))
+        print("question:           ",question)
+        print("rewritten_question: ",rewritten_question)
    
-    if TEST: print("questions: ", q)
     #/ embed question
     query_embedding = embed_text(rewritten_question)
  
     ##SOURCES
     #/ get the best sources
     chunks = retrieve_chunks(query_embedding)
- 
     sources = trim_chunks_by_tokens(chunks)
  
     #/ format context for the LLM
@@ -386,23 +447,37 @@ def chat(user_id, question):
  
     #/ get chat history
     chat_history = context_history(chat_id)
- 
-    if TEST: print("\n context_text: ", context_text)
-    if TEST: print("\n question: ", question)
+    
+    #/ build messages
     input = messages(chat_history, context_text, question)
  
-    if TEST: print("input: ", input)
+    # if TEST: 
+    #     print("input: ", (input))
  
     ##Answer
     #/ get response
-    completion = client.chat.completions.create(
+    completion = client.responses.create(
         model="gpt-4o-mini",
-        temperature=0,
-        messages=input
+        temperature=TEMP,
+        input=input
     )
-    answer = completion.choices[0].message.content.strip()
+
+    answer = completion.output_text.strip()
  
-    if TEST: print("answer: ",answer)
+    if TEST: print("\n answer: ",answer)
+
+    ## Update DB
+
+    #/ store question
+    question_tokens = len(tokenizer.encode(question))
+
+    q = supabase.table("chat_messages").insert({
+        "chat_id": chat_id,
+        "role": "user",
+        "content": question,
+        "token_count": question_tokens,
+        "time_stamp": local_time
+    }).execute()
  
     #/ store answer
     answer_time = datetime.now().strftime('%I:%M:%S %p')
@@ -413,7 +488,7 @@ def chat(user_id, question):
         "chat_id": chat_id,
         "role": "assistant",
         "content": answer,
-        "time_stamp": local_time,
+        "time_stamp": answer_time,
         "token_count": answer_tokens,
         "sources": sources_id
         }).execute()
@@ -421,3 +496,34 @@ def chat(user_id, question):
     message = {"answer": answer, "question": question, "sources": sources, "chat_id": chat_id}
  
     return message
+
+def test():
+    #embed_all()
+    user_id = "73205ea6-2afe-4407-a35e-6ea6f7260333"
+    question = "do we learn instana"
+    convo  = chat(user_id, question)
+    q = convo["question"]
+    a = convo["answer"]
+    print("\n number of sources:", len(convo["sources"]))
+
+    prompt = f"""
+        A bot is used to answer trainees' questions based on provided document context.
+        Rate the question and answer pair out of 100. give a short explanation. Briefly suggest a better question if necessary and ways to improve the bot.
+        structure it as:
+          rating: <score out of 100>
+          explanation: <short explanation>
+          suggested_bot_improvements: <suggestions>
+          suggested_question_improvements: <suggestions>
+ 
+        Question: {q}
+        Answer: {a}
+        """.strip()
+ 
+    response = client.responses.create(
+        model="gpt-4o-mini",
+        input=prompt
+    )
+
+    print("\n",response.output_text.strip())
+
+test()
